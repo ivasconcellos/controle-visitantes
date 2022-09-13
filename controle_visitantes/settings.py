aabf12b32cb322fd258f627e52d53356cc52099e
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2022-09-13 11:50:00 -0300

    Configured templates in settings.py

--- controle_visitantes/settings.py
+++ controle_visitantes/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'controle_visitantes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [ Path(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
