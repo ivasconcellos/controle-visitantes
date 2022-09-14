85ea7af973dd90daa8a348d21738e0e108e5ddcd
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2022-09-13 21:52:48 -0300

    Created models e migration for visitantes.

--- visitantes/models.py
+++ visitantes/models.py
@@ -1,3 +1,26 @@
+from tabnanny import verbose
 from django.db import models
 
-# Create your models here.
+class Visitante(models.Model):
+
+    nome_completo = models.CharField(verbose_name="Nome completo do visitante", max_length=194)
+    cpf = models.CharField(verbose_name="CPF", max_length=11)
+    data_nascimento = models.DateField(verbose_name="Data de nascimento", auto_now=False, auto_now_add=False)
+    numero_cada = models.PositiveBigIntegerField(verbose_name="Número da casa")
+    placa_veiculo = models.CharField(verbose_name="Placa do veículo", max_length=7, blank=True, null=True)
+    horario_chegada = models.DateTimeField(verbose_name="Horário de chegada", auto_now_add=True)
+    horario_saida = models.DateTimeField(verbose_name="Horário de saída", auto_now=False, blank=True, null=True)
+    horario_autorizacao = models.DateTimeField(verbose_name="Horário de autorização da entrada", auto_now=False, blank=True, null=True)
+    morador_responsavel = models.CharField(verbose_name="Morador responsável por autorizar a entrada do visitante", max_length=194, blank=True)
+    registrado_por = models.ForeignKey("porteiros.Porteiro", verbose_name="Reponsável pelo registro", on_delete=models.PROTECT)
+
+
+    class Meta:
+        verbose_name = "Visitante"
+        verbose_name_plural = "Visitantes"
+        db_table = "visitante"
+
+    def __str__(self):
+        return self.nome_completo
+
+
