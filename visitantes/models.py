b7d3245b692a7d1dc134546b05d9964988afc0fe
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2022-09-13 23:52:46 -0300

    Configured informacoes_visitante.

--- visitantes/models.py
+++ visitantes/models.py
@@ -14,6 +14,25 @@ class Visitante(models.Model):
     morador_responsavel = models.CharField(verbose_name="Morador responsável por autorizar a entrada do visitante", max_length=194, blank=True)
     registrado_por = models.ForeignKey("porteiros.Porteiro", verbose_name="Reponsável pelo registro", on_delete=models.PROTECT)
 
+    def get_horario_saida(self):
+        if self.horario_saida:
+            return self.horario_saida
+        return "Horário de saída não registrado!"
+
+    def get_horario_autorizacao(self):
+        if self.horario_autorizacao:
+            return self.horario_autorizacao
+        return "Visitante aguardando autorização!"
+
+    def get_morador_responsavel(self):
+        if self.morador_responsavel:
+            return self.morador_responsavel
+        return "Visitante aguardando autorização!"
+
+    def get_placa_veiculo(self):
+        if self.placa_veiculo:
+            return self.placa_veiculo
+        return "Veículo não registrado!"
 
     class Meta:
         verbose_name = "Visitante"
--- visitantes/views.py
+++ visitantes/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Visitante
 from .forms import VisitanteForm
 
 
@@ -25,3 +26,12 @@ def registrar_visitante(request):
         }
     return render(request, 'registrar_visitante.html', context)
 
+
+def informacoes_visitante(request, id):
+
+    visitante = get_object_or_404 (Visitante, id=id)
+    context = { 
+        'nome_pagina': 'Informações de visitante',
+        'visitante': visitante
+        }
+    return render(request, 'informacoes_visitante.html', context)
