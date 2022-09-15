d7f404921ffe04743e8b2d86d66131a3dc480aca
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2022-09-15 00:32:25 -0300

    Configured logout.

--- controle_visitantes/urls.py
+++ controle_visitantes/urls.py
@@ -8,6 +8,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(template_name="logout.html"), name="logout"),
     path("", index, name="index"),
     path("registrar_visitante/", registrar_visitante, name="registrar_visitante"),
     path("visitantes/<int:id>/", informacoes_visitante, name="informacoes_visitante"),
